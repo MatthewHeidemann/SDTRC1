8951680ca11ecead2c399b431be6992a8204bb96
MatthewHeidemann <matt.heidemann@gmail.com>
2025-05-08 23:29:31 -0700

    Add files via upload

--- firebase.js
+++ firebase.js
@@ -1,13 +1,15 @@
 
+// firebase.js
 const firebaseConfig = {
   apiKey: "AIzaSyBegIGyaR1tnG_u2BY9FKm9KP36JSPaPts",
   authDomain: "sdtrc1.firebaseapp.com",
   databaseURL: "https://sdtrc1-default-rtdb.firebaseio.com",
   projectId: "sdtrc1",
-  storageBucket: "sdtrc1.firebasestorage.app",
+  storageBucket: "sdtrc1.appspot.com",
   messagingSenderId: "461359871112",
   appId: "1:461359871112:web:1081c160384d476131db3d"
 };
 
-firebase.initializeApp(firebaseConfig);
+const app = firebase.initializeApp(firebaseConfig);
 const db = firebase.database();
+const firebaseReady = Promise.resolve();
